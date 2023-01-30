ff975f846d3760d09bf10cf7c9df923bbdad3f7e
andreatrentini <55395467+andreatrentini@users.noreply.github.com>
2023-01-30 10:18:55 +0000

    Aggiunta la classe profilo

--- reactive-forms/src/app/editor-profilo-fb/editor-profilo-fb.component.ts
+++ reactive-forms/src/app/editor-profilo-fb/editor-profilo-fb.component.ts
@@ -1,59 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { ProfiloService } from '../profilo.service';
+import { Profilo } from '../profilo';
 
 @Component({
   selector: 'app-editor-profilo-fb',
   templateUrl: './editor-profilo-fb.component.html',
   styleUrls: ['./editor-profilo-fb.component.css']
 })
-export class EditorProfiloFbComponent implements OnInit{
+export class EditorProfiloFbComponent implements OnInit {
 
-  profilo!: any;
+  profilo: Profilo;
   formProfilo!: any;
 
-  constructor(private fb: FormBuilder, private ps: ProfiloService) {}
-  
+  constructor(private fb: FormBuilder) {
+    this.profilo = new Profilo();
+  }
+
   ngOnInit() {
-    let profiloLetto = this.ps.carica();
-    console.log(profiloLetto);
+    this.profilo.carica();
 
-    if (profiloLetto) {
-      this.profilo = profiloLetto;
-    }
-    else {
-      this.profilo = {
-        nome: '',
-        cognome: '',
-        email: '',
-        indirizzo: {
-          via: '',
-          nCivico: '',
-          cap: '',
-          citta: '',
-          provincia: ''
-        }
-      }
-    }
+    this.formProfilo = this.profilo.createForm();
+    console.log('profilo', this.profilo);
+  }
 
-    
-    this.formProfilo = this.fb.group({
-      nome: [this.profilo.nome, Validators.required],
-      cognome: [this.profilo.cognome],
-      email: [this.profilo.email, [Validators.required, Validators.email]],
-      indirizzo: this.fb.group({
-        via: [this.profilo.indirizzo.via],
-        nCivico: [this.profilo.indirizzo.nCivico],
-        cap: [this.profilo.indirizzo.cap, Validators.required],
-        citta: [this.profilo.indirizzo.citta, Validators.required],
-        provincia: [this.profilo.indirizzo.provincia, Validators.required]
-      })
-    });
-      console.log('profilo', this.profilo);
-    }
-  
   salva() {
     console.log(this.formProfilo.value);
-    this.ps.salva(this.formProfilo.value);
+    this.profilo.salva(this.formProfilo.value);
   }
 }
